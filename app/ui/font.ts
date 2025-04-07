7f4875bb553d859b32a3a35011c3d239f6f23cd2
noellyudistiraa <masnoelorangjawa@gmail.com>
2025-04-07 14:32:22 +0700

    Update font.ts

--- app/ui/font.ts
+++ app/ui/font.ts
@@ -1,4 +1,4 @@
-import { Inter, Lusitana, Poppins, Shadows_Into_Light_Two } from 'next/font/google';
+import { Inter, Lusitana, Poppins, Shadows_Into_Light_Two, New_Rocker } from 'next/font/google';
 
 export const inter = Inter({
     subsets: ['latin'],
@@ -15,7 +15,7 @@ export const lusitana = Lusitana({
   export const poppins = Poppins({
     subsets: ['latin'],
     display: 'swap',
-    weight: ['400', '500', '600'],
+    weight: ['400', '500', '600', '700'],
     variable: '--font-poppins',
   });
   
@@ -25,3 +25,10 @@ export const lusitana = Lusitana({
     display: 'swap',
     variable: '--font-shadows',
   });
+
+  export const newRocker = New_Rocker({  
+    weight: ['400'], // New Rocker biasanya hanya memiliki satu bobot  
+    subsets: ['latin'],  
+    display: 'swap',  
+    variable: '--font-new-rocker', // Tambahkan variable untuk CSS custom property  
+});  
